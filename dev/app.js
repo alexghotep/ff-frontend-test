955ccfd8e86f7cfdafe4a8ca680e0ec6e4bbbfff
a.kutuzov <3XTGxkFl9XPm>
2022-02-23 17:58:59 +0300

    ff-frontend test project websocket connected

--- dev/app.js
+++ dev/app.js
@@ -8,50 +8,20 @@ ws.onopen = function () {
 };
 
 
-// Обрабатываем входящее сообщение
-ws.onmessage = function (m) {
-    const [event, data] = JSON.parse(m.data);
-    console.log(event, data)
+ws.onopen = function() { // Ждём открытия соединения
+    ws.send(JSON.stringify(['quotes', ['RSTI', 'GAZP', 'MRKZ', 'RUAL', 'HYDR', 'MRKS', 'SBER', 'FEES', 'TGKA', 'VTBR', 'ANH.US', 'VICL.US', 'BURG.US', 'NBL.US', 'YETI.US', 'WSFS.US', 'NIO.US', 'DXC.US', 'MIC.US', 'HSBC.US', 'EXPN.EU', 'GSK.EU', 'SHP.EU', 'MAN.EU', 'DB1.EU', 'MUV2.EU', 'TATE.EU', 'KGF.EU', 'MGGT.EU', 'SGGD.EU']]));
 };
 
-// Обрабатываем закрытие соединения
-ws.onclose = function (e) {
-    console.log('sockets closed', e);
-};
-/*
-// Обрабатываем ошибку
-ws.onerror = function (error) {
-    console.log('Sockets.error: ', error);
-    ws.close();
-};
-
-var tickersToWatchChanges = ['RSTI', 'GAZP', 'MRKZ', 'TATN', 'HYDR', 'MRKS', 'SBER','FEES', 'TGKA', 'VTBR','ANH.US', 'VICL.US', 'BURG.US', 'NBL.US', 'YETI.US', 'WSFS.US', 'NIO.US', 'DXC.US','MIC.US', 'HSBC.US', 'EXPN.EU', 'GSK.EU', 'SHP.EU', 'MAN.EU', 'DB1.EU', 'MUV2.EU', 'TATE.EU', 'KGF.EU', 'MGGT.EU', 'SGGD.EU'];
-// console.log(tickersToWatchChanges);*/
-
-/**
- * @param QuoteInfoAnswer[] data
- */
-/*function updateWatcher(data) {
-    data.forEach(console.info.bind(console));
+// Обработка входящих данных
+function quotesHandler(data) {
+    console.log(data);
 }
 
+const handlers = {
+    q: quotesHandler
+};
+
 ws.onmessage = function (m) {
     const [event, data] = JSON.parse(m.data);
-    if (event === 'q') {
-        updateWatcher(data);
-    }
+    handlers[event](data);
 };
-ws.onopen = function() { // Ждём открытия соединения
-    // ws.send("quotes", JSON.stringify(['quotes', tickersToWatchChanges]));
-};
-
-(function websocketStart() {
-    const ws = new WebSocket(WebSocketsURL);
-
-    ws.onclose = function (e) {
-        console.log('sockets closed', e);
-        setTimeout(function () {
-            websocketStart();
-        }, 5000); // Попробуем подключиться через 5 секунд после разрыва
-    };
-})()*/
