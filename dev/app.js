ceb4191fad219c4fd0c002c196100923e86b4890
a.kutuzov <3XTGxkFl9XPm>
2022-02-23 17:37:17 +0300

    ff-frontend test project websocket connected

--- dev/app.js
+++ dev/app.js
@@ -18,20 +18,20 @@ ws.onmessage = function (m) {
 ws.onclose = function (e) {
     console.log('sockets closed', e);
 };
-
+/*
 // Обрабатываем ошибку
 ws.onerror = function (error) {
     console.log('Sockets.error: ', error);
     ws.close();
 };
 
-var tickersToWatchChanges = ['RSTI', 'GAZP', 'MRKZ', 'TATN', 'HYDR', 'MRKS', 'SBER', 'FEES', 'TGKA', 'VTBR', 'ANH.US', 'VICL.US', 'BURG.US', 'NBL.US', 'YETI.US', 'WSFS.US', 'NIO.US', 'DXC.US', 'MIC.US', 'HSBC.US', 'EXPN.EU', 'GSK.EU', 'SHP.EU', 'MAN.EU', 'DB1.EU', 'MUV2.EU', 'TATE.EU', 'KGF.EU', 'MGGT.EU', 'SGGD.EU'];
-console.log(tickersToWatchChanges);
+var tickersToWatchChanges = ['RSTI', 'GAZP', 'MRKZ', 'TATN', 'HYDR', 'MRKS', 'SBER','FEES', 'TGKA', 'VTBR','ANH.US', 'VICL.US', 'BURG.US', 'NBL.US', 'YETI.US', 'WSFS.US', 'NIO.US', 'DXC.US','MIC.US', 'HSBC.US', 'EXPN.EU', 'GSK.EU', 'SHP.EU', 'MAN.EU', 'DB1.EU', 'MUV2.EU', 'TATE.EU', 'KGF.EU', 'MGGT.EU', 'SGGD.EU'];
+// console.log(tickersToWatchChanges);*/
 
 /**
  * @param QuoteInfoAnswer[] data
  */
-function updateWatcher(data) {
+/*function updateWatcher(data) {
     data.forEach(console.info.bind(console));
 }
 
@@ -41,6 +41,17 @@ ws.onmessage = function (m) {
         updateWatcher(data);
     }
 };
-ws.onopen = function () { // Ждём открытия соединения
-    ws.send("quotes", JSON.stringify(['quotes', tickersToWatchChanges]));
-}
+ws.onopen = function() { // Ждём открытия соединения
+    // ws.send("quotes", JSON.stringify(['quotes', tickersToWatchChanges]));
+};
+
+(function websocketStart() {
+    const ws = new WebSocket(WebSocketsURL);
+
+    ws.onclose = function (e) {
+        console.log('sockets closed', e);
+        setTimeout(function () {
+            websocketStart();
+        }, 5000); // Попробуем подключиться через 5 секунд после разрыва
+    };
+})()*/
